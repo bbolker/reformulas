221440245cb87cd628c76bc49b84571c3d2b6884
Ben Bolker <bbolker@gmail.com>
2024-10-15 12:00:03 -0400

    tests for 'naked s' problem

---
+++ inst/tinytest/test_anySpecial.R
@@ -0,0 +1,10 @@
+library(reformulas)
+library(tinytest)
+
+## shouldn't find 'naked' specials
+expect_false(anySpecial(y ~ s))
+expect_true(anySpecial(y ~ s(1)))
+expect_false(anySpecial(y ~ s[[1]]))
+expect_false(anySpecial(y ~ diag))
+expect_true(anySpecial(y ~ diag(1)))
+expect_false(anySpecial(y ~ diag[[1]]))
